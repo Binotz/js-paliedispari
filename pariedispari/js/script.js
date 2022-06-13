d7d14c8e572a5d9d3d9201f50de7b07f4399f6fc
Binotz <binotto.riccardo@gmail.com>
2022-06-13 15:06:48 +0200

    Pari e dispari done

---
+++ pariedispari/js/script.js
@@ -0,0 +1,38 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+//dichiarazione variabili
+let userEvenOrOdd = prompt('inserisci o pari o dispari').toLowerCase();
+const userNumberInput = parseInt(prompt('Inserisci un numero'));
+const pcNumber = randomGenerator(1,5);
+
+//stampo i dati raccolti
+console.log('Utente ha scelto', userEvenOrOdd);
+console.log('Numero scelto dall utente', userNumberInput);
+console.log('Numero scelto per il pc', pcNumber);
+
+//stampo l'esito del gioco
+if(evenOrOdd(userNumberInput, pcNumber) === userEvenOrOdd){
+    console.log('utente ha vinto!');
+} else {
+    console.log('pc ha vinto!')
+}
+
+//Sezione funzioni
+function randomGenerator(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function evenOrOdd(num1, num2){
+    let sum = num1 + num2;
+    console.log("somma", sum)
+    if(sum % 2 === 0){
+        return 'pari'
+    } else {
+        return 'dispari'
+    }
+}
